7b055c50696230373e763fb63cfaf52f81d62c31
ThaisMartins18 <thaisfmartins18@gmail.com>
2023-06-13 18:09:32 -0300

    2.1

--- src/React/Atv02/counter.js
+++ src/React/Atv02/counter.js
@@ -3,6 +3,9 @@ import Buttons from "./buttons"
 import './counter.css'
 import kushima from './kushima.png'
 import minato from './minato.jpg'
+import Container from "@mui/material/Container";
+import { Box } from "@mui/material";
+
 
 export function Counter() {
 
@@ -20,31 +23,33 @@ export function Counter() {
     }
 
     return (
-  <div class="avatar-container">
-    <div class="total-container">
-      <h2 class="total-button-container">
-        <span>Total: {total}</span>
-        <button class="reset-button" onClick={Zerar}>Reset</button>
-      </h2>
-    </div>
-    <div>
-      <img src={minato} alt={'Avatar Man'} class="avatar" />
-      <div class="counter">
-        <Buttons status='add' onClick={() => setQtdman(qtdman + 1)} />
-        <p class="count">{qtdman}</p>
-        <Buttons status='decrementar' onClick={() => setQtdman(qtdman - 1)} />
-      </div>
-    </div>
-    <div>
-      <img src={kushima} alt={'Avatar Woman'} class="avatar" />
-      <div class="counter">
-        <Buttons status='add' onClick={() => setQtdwoman(qtdwoman + 1)} />
-        <p class="count">{qtdwoman}</p>
-        <Buttons status='decrementar' onClick={() => setQtdwoman(qtdwoman - 1)} />
-      </div>
-    </div>
-  </div>
-)
+        <div class="grade">
+            <div class="avatar-container">
+                <div class="total-container">
+                    <h2 class="total-button-container">
+                        <span>Total: {total}</span>
+                        <button class="reset-button" onClick={Zerar}>Reset</button>
+                    </h2>
+                </div>
+                <div>
+                    <img src={minato} alt={'Avatar Man'} class="avatar" />
+                    <div class="counter">
+                        <Buttons status='add' onClick={() => setQtdman(qtdman + 1)} />
+                        <p class="count">{qtdman}</p>
+                        <Buttons status='decrementar' onClick={() => setQtdman(qtdman - 1)} />
+                    </div>
+                </div>
+                <div>
+                    <img src={kushima} alt={'Avatar Woman'} class="avatar" />
+                    <div class="counter">
+                        <Buttons status='add' onClick={() => setQtdwoman(qtdwoman + 1)} />
+                        <p class="count">{qtdwoman}</p>
+                        <Buttons status='decrementar' onClick={() => setQtdwoman(qtdwoman - 1)} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 
 }
 export default Counter;
